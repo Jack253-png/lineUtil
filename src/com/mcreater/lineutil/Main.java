8b179e7ad3841ba37edc2def546c89643df0f35b
Jack253-png <guoxiuchen20170402@163.com>
2023-01-21 21:14:02 +0800

    updated on 2023.1.21

---
+++ src/com/mcreater/lineutil/Main.java
@@ -0,0 +1,11 @@
+package com.mcreater.lineutil;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> arguments = Arrays.asList(args);
+        System.out.println(arguments);
+    }
+}
